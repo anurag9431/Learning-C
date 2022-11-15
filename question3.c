b9cf06c05f52889f65d4d457dcbc6bc5436abaac
anurag9431 <anuragdubey9431@gmail.com>
2022-11-15 22:50:11 +0530

    added some question

---
+++ question3.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main()
+{
+printf("Anurag\n123456\nECSc\n56\nmale\n74657474\nranchi");return 0;
+}
---
+++ question6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float base,area,height;
+    printf("enter the base of a triangle");
+    printf("enter the height of a triangle");
+    scanf("%f",&area);
+    area=1/2*base*height;
+    printf("area of a triangle:");
+    scanf("%f",&area);
+    return 0;
+}
